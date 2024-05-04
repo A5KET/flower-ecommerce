c7c2ee4eeeee98575443a93297a629f67da2eb5d
A5KET <asketinin@gmail.com>
2024-05-04 11:54:31 +0300

    Added eslint

---
+++ eslint.config.js
@@ -0,0 +1,21 @@
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import stylisticJs from '@stylistic/eslint-plugin-js'
+
+
+export default [
+  { languageOptions: { globals: { ...globals.browser, ...globals.node, ...globals.mocha } } },
+  pluginJs.configs.recommended,
+  {
+    plugins: {
+
+      '@stylistic/js': stylisticJs
+    },
+    rules: {
+      indent: ['error', 2],
+      semi: ['error', 'never'],
+      quotes: ['error', 'single'],
+      '@stylistic/js/object-curly-spacing': ['error', 'always'],
+    }
+  }
+]
