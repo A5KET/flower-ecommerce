657960e7055cb95581f6e31c22957d1a88dd69a1
A5KET <asketinin@gmail.com>
2024-05-15 17:38:25 +0300

    Fixed undefined date as parameter in convertToDateTimeLocalString

--- static/js/formats.js
+++ static/js/formats.js
@@ -1,4 +1,8 @@
 export function convertToDateTimeLocalString(date) {
+  if (!date) {
+    return undefined
+  }
+
   const year = date.getFullYear()
   const month = (date.getMonth() + 1).toString().padStart(2, '0')
   const day = date.getDate().toString().padStart(2, '0')
