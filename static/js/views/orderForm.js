fe7b0ed3cf975718c517f8d8ecedadbbf2547573
A5KET <asketinin@gmail.com>
2024-05-17 17:20:16 +0300

    Added form to add new products in OrderForm

--- static/js/views/orderForm.js
+++ static/js/views/orderForm.js
@@ -3,14 +3,22 @@ import { Fieldset, FormButtons, TextInputField, DateTimeField, SelectField } fro
 import { createElement } from '../layout.js'
 
 
-function ProductListElement(product) {
+function ProductListElement(product, onRemove) {
+  function onRemoveButtonClick(event) {
+    onRemove(product)
+  }
+
   return createElement(
     { tag: 'div', className: 'product' },
     [
       createElement({ tag: 'span', className: 'product-name', textContent: product.name }),
-      createElement({ tag: 'span', textContent: `${product.price} гр/шт x ${product.amount} = ${product.price * product.amount} грн.` }),
       createElement(
-        { tag: 'button', className: 'product-remove' },
+        {
+          tag: 'span',
+          textContent: `${product.price} гр/шт x ${product.amount} = ${product.price * product.amount} грн.`,
+        }),
+      createElement(
+        { tag: 'button', className: 'product-button', onclick: onRemoveButtonClick },
         [
           createElement({ tag: 'img', src: '/img/cross.svg' })
         ]
@@ -19,16 +27,57 @@ function ProductListElement(product) {
   )
 }
 
-function ProductList(products) {
-  const totalPrice = products.reduce((cur, product) => cur + (product.price * product.amount), 0)
+function ProductListElementForm(onSubmit) {
+  function onFormSubmit(event) {
+    event.preventDefault()
+    const data = new FormData(event.target)
+
+    onSubmit(Object.fromEntries(data))
+  }
 
   return createElement(
+    { tag: 'form', className: 'product product-form', onsubmit: onFormSubmit },
+    [
+      createElement({ tag: 'input', className: 'product-name', name: 'name', placeholder: 'Назва товару...', required: true }),
+      createElement({ tag: 'input', name: 'amount', placeholder: '- - -', required: true, size: 1, type: 'number', min: 0 }),
+      'гр/шт x ',
+      createElement({ tag: 'input', name: 'price', placeholder: '- - -', required: true, size: 1, type: 'number', min: 0 }),
+      createElement(
+        { tag: 'button', className: 'product-button', type: 'submit' },
+        [
+          createElement({ tag: 'img', src: '/img/add.svg' })
+        ]
+      )
+    ]
+  )
+}
+
+
+/** @param {Array} products */
+function ProductList(products) {
+  function onProductListAdding(product) {
+    products.push(product)
+    node.replaceWith(ProductList(products))
+  }
+
+  function onProductRemoving(product) {
+    const index = products.indexOf(product)
+    products.splice(index, 1)
+    node.replaceWith(ProductList(products))
+  }
+
+  const totalPrice = products.reduce((cur, product) => cur + (product.price * product.amount), 0)
+
+  const node = createElement(
     { tag: 'div', className: 'products' },
     [
-      ...products.map(product => ProductListElement(product)),
+      ...products.map(product => ProductListElement(product, onProductRemoving)),
+      ProductListElementForm(onProductListAdding),
       createElement({ tag: 'span', className: 'total-price', textContent: `Повна ціна: ${totalPrice} грн.` })
     ]
   )
+
+  return node
 }
 
 
@@ -39,21 +88,24 @@ function OrderFormMain(order, statusOptions) {
         id: 'status',
         options: statusOptions,
         active: order.status,
-        label: 'Статус:'
+        label: 'Статус:',
+        required: true 
       }
     ),
     TextInputField(
       {
         id: 'customer',
         value: order.customer,
-        label: 'Замовник:'
+        label: 'Замовник:',
+        required: true
       }
     ),
     DateTimeField(
       {
         id: 'timeCreated',
         value: order.timeCreated || new Date(),
-        label: 'Дата створення:'
+        label: 'Дата створення:',
+        required: true
       })
   ]
 
@@ -74,5 +126,5 @@ export function OrderForm(order, statusOptions) {
 }
 
 export function NewOrderForm(statusOptions) {
-  return OrderForm({ }, statusOptions)
+  return OrderForm({}, statusOptions)
 }
