4ee0fd1878cf0c358bf9022595d437a95ef0d4dd
A5KET <asketinin@gmail.com>
2024-05-21 10:24:29 +0300

    Added comments and react-like event listener properties

--- static/js/layout.js
+++ static/js/layout.js
@@ -1,3 +1,16 @@
+/**
+ * 
+ * @param {string} key 
+ * @returns 
+ */
+const isEvent = key => key.startsWith('on')
+
+/**
+ * 
+ * @param {Object} object 
+ * @param {function(any): boolean} predicate 
+ * @returns 
+ */
 export function filterProperties(object, predicate) {
   const filteredObject = {}
 
@@ -13,19 +26,47 @@ export function filterProperties(object, predicate) {
 }
 
 
+/**
+ * 
+ * @param {Object} object 
+ * @returns 
+ */
 export function filterUndefinedProperties(object) {
   return filterProperties(object, property => property !== undefined)
 }
 
 
-export function createElement(props, children=[]) {
+/**
+ * 
+ * @param {Object} props 
+ * @param {HTMLElement} node 
+ */
+function addEventListeners(props, node) {
+  Object.keys(props).filter(isEvent)
+    .forEach(property => {
+      const eventType = property.toLowerCase().substring(2)
+      node.addEventListener(eventType, props[property])
+    })
+}
+
+
+/**
+ * 
+ * @param {LayoutElement} props 
+ * @param {(LayoutElement|string)[]} children 
+ * @returns {HTMLElement}
+ */
+export function createElement(props, children = []) {
   const node = document.createElement(props.tag)
   const filteredProps = filterUndefinedProperties(props)
+
+  addEventListeners(filteredProps, node)
 
   Object.assign(node, filteredProps)
 
   for (const child of children) {
     const childNode = typeof child === 'object' ? child : createTextElement(child)
+    // @ts-ignore
     node.appendChild(childNode)
   }
 
@@ -33,11 +74,20 @@ export function createElement(props, children=[]) {
 }
 
 
+/**
+ * 
+ * @param {string} text 
+ */
 export function createTextElement(text) {
   return document.createTextNode(text)
 }
 
 
+/**
+ * 
+ * @param {string} href 
+ * @returns 
+ */
 export function Style(href) {
   return createElement(
     { tag: 'link', rel: 'stylesheet', href }
@@ -45,8 +95,14 @@ export function Style(href) {
 }
 
 
-export function mountLayout(layout, title, styles=[]) {
-  styles.map(style => document.head.appendChild(Style(style)))
+/**
+ * 
+ * @param {HTMLElement} layout 
+ * @param {string} title 
+ * @param {string[]} styleSources 
+ */
+export function mountLayout(layout, title, styleSources = []) {
+  styleSources.forEach(style => document.head.appendChild(Style(style)))
   document.body.replaceWith(layout)
   document.title = title
 }
